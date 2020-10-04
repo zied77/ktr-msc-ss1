78b9b37875c71af5ae0225dfab5f79a38f9c68cb
zied77 <44898085+zied77@users.noreply.github.com>
2020-10-04 19:19:23 +0100

    Add files via upload

---
+++ Mage.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author zied
+ */
+public class Mage extends Character{
+    public Mage(){
+        Warrior.RPGClass = "Mage";
+        setLife(70);
+        setAgility(10);
+        setStrength(3);
+        setWit(10);
+    }
+    
+    public Mage(String name){
+        super(name);
+                System.out.println(name+" : May the gods be with me .");
+
+    }
+    
+
+    @Override
+    public void attack(String weapon) {
+        if (weapon.equalsIgnoreCase("magic") || weapon.equalsIgnoreCase("wand") ) {
+            super.attack(weapon);
+            System.out.println(getName()+" : Feel the power of my "+weapon+" !"); 
+                       
+        }
+         //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void moveRight() {
+        System.out.println(getName()+" : moves right furtively .");
+    }
+
+    @Override
+    public void moveBack() {
+        System.out.println(getName()+" : moves back furtively .");
+    }
+
+    @Override
+    public void moveForward() {
+        System.out.println(getName()+" : moves forward furtively .");
+    }
+
+    @Override
+    public void moveLeft() {
+        System.out.println(getName()+" : moves left furtively .");
+    }
+    
+}
---
+++ Warrior.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author zied
+ */
+public class Warrior extends Character {
+    public Warrior(){
+        Warrior.RPGClass = "Warrior";
+        setLife(100);
+        setAgility(8);
+        setStrength(10);
+        setWit(3);
+    }
+
+    @Override
+    public void attack(String weapon) {
+        if (weapon.equalsIgnoreCase("hammer") || weapon.equalsIgnoreCase("sword") ) {
+            super.attack(weapon);
+            System.out.println(getName()+" : I'll crush you with my"+weapon+" !"); 
+                   
+                    
+                  
+        }
+         //To change body of generated methods, choose Tools | Templates.
+    }
+    
+     public Warrior(String name){
+        super(name);
+                System.out.println(name+" :  My name will go down in history !");
+    }
+
+    @Override
+    public void moveRight() {
+        System.out.println(getName()+" : moves right like a bad boy");
+    }
+
+    @Override
+    public void moveBack() {
+        System.out.println(getName()+" : moves back like a bad boy");
+    }
+
+    @Override
+    public void moveForward() {
+        System.out.println(getName()+" : moves forward like a bad boy");
+    }
+
+    @Override
+    public void moveLeft() {
+        System.out.println(getName()+" : moves left like a bad boy");
+    }
+    
+    
+    
+    
+    
+}
